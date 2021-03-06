5130ea509681fc9b38690fc2607ad20a7e8ca00f
avishaiDV <39707337+avishaiDV@users.noreply.github.com>
2021-03-06 20:22:49 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const socket = require("socket.io");
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 const server = app.listen(PORT, function () {
   console.log("listening to requests on port ", PORT);
 });
